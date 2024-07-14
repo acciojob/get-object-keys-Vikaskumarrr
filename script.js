c38c716474636f24f8e29d26416b5a8f46250c73
vikas Bhnadari <143922604+Vikaskumarrr@users.noreply.github.com>
2024-07-14 13:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 //your JS code here. If required.
 let student = { 
     name: "vikas",
-	getkeys : function(){ 
-    return Object.keys(this);
-		}
+	// getkeys : function(){ 
+ //    return Object.keys(this);
+	// 	}
 }
 
 // Object.prototype.getkeys = function(){ 
 //     return Object.keys(this);
 // }
 
-const key = student.getkeys();
-console.log(key);
+// const key = student.getkeys();
+// console.log(key);
+
+
+function getKeys(obj) {
+    return Object.keys(obj);
+}
+const keys = getKeys(student);
+console.log(keys);
